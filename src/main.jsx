dd088cc5ad2434b9c110b341fe4001c832b9bc9e
amrkhaled104 <amrkhaledelsisy@gmail.com>
2026-05-12 05:34:36 +0300

    global configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+import { Toaster } from 'react-hot-toast';
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Toaster position="top-right" reverseOrder={false} />
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
